f292aa417fadad4818212e9862547ca5dca8d6df
kato <y-kato@miharu.co.jp>
2020-12-04 16:41:04 +0900

    Add one button for rating

---
+++ FoodTracker/RatingControl.swift
@@ -0,0 +1,45 @@
+//
+//  RatingControl.swift
+//  FoodTracker
+//
+//  Created by 加藤　康久 on 2020/12/04.
+//
+
+import UIKit
+
+class RatingControl: UIStackView {
+    //MARK: initialize
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupButtons()
+    }
+    
+    required init(coder: NSCoder) {
+        super.init(coder: coder)
+        setupButtons()
+    }
+    
+    //MARK: Button Action
+    @objc func ratingButtonTapped(button: UIButton) {
+        print("👍")
+    }
+    
+    //MARK: Private Methods
+    private func setupButtons() {
+        // Create the button
+        let button = UIButton()
+        button.backgroundColor = UIColor.red
+        
+        // Add constraints
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.heightAnchor.constraint(equalToConstant: 44.0).isActive = true
+        button.widthAnchor.constraint(equalToConstant: 44.0).isActive = true
+        
+        // Setup the button action
+        button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(button:)), for: .touchUpInside)
+        
+        // ADD the button to the stack
+        addArrangedSubview(button)
+        
+    }
+}
