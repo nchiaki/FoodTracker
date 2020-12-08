ee34e26e20c99061fc7e4215b70f1258db2c9215
kato <y-kato@miharu.co.jp>
2020-12-08 17:19:32 +0900

    Add new file before "Load Initial Data" of Create a Table view

---
+++ FoodTracker/MealTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MealTableViewCell.swift
+//  FoodTracker
+//
+//  Created by 加藤　康久 on 2020/12/08.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
